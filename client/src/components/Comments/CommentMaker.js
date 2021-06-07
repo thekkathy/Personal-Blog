327fe2334597e9c5cff1156a19d6ffaa87d52515
unknown <cdg5be@virginia.edu>
2021-06-07 17:27:12 -0400

    end of day 1 commit

--- client/src/components/Comments/CommentMaker.js
+++ client/src/components/Comments/CommentMaker.js
@@ -1,12 +1,31 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { CommentsContext } from '...../context/CommentsContext';
+import { CommentsContext } from '../../context/commentsContext';
+import { UsersContext } from '../../context/usersContext';
 
 const CommentMaker = () => {
     
     const { comments, setComments } = useContext(CommentsContext);
+    const { user } = useContext(UsersContext);
     
     return (
         <div>
+            {user && (
+                <div>
+                    <p>Create New Comment</p>
+                    <div class="container">
+                        <div class="row">
+                            <form>
+
+                            </form>
+                        </div>
+                        <div class="row">
+                            <button>
+                                
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
             CommentMaker
         </div>
     )
