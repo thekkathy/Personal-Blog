4ee7261d274384dd426de19429510580306f9d5c
Dhruv Batra <dhruvbatra2@gmail.com>
2021-06-08 15:13:13 -0400

    profile page

--- client/src/components/auth/UserPage.js
+++ client/src/components/auth/UserPage.js
@@ -1,10 +1,17 @@
 import SignOut from './SignOut'
+import {useContext } from "react";
+import {UsersContext} from '../../context/usersContext'
 
 export default function UserPage(){
+
+    const { users, setUsers } = useContext(UsersContext);
+
     return(
         <div>
+            <h1>Welcome {users.displayName}</h1>
+            <img src={users.photoURL}></img>
+            <p>Email: {users.email}</p>
             <SignOut/>
-            <p>user page</p>
         </div>
     )
 }
