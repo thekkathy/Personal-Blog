abfae01282e49d1c6c5e34528cb9c3dd84bc036a
Dhruv Batra <dhruvbatra2@gmail.com>
2021-06-08 18:38:23 -0400

    merge

--- server/blog_posts/blog_posts.js
+++ server/blog_posts/blog_posts.js
@@ -20,13 +20,49 @@ app.get("/blog_posts/get", async (req, res) => {
   res.json(blogPosts);
 });
 
-app.get("/blog_posts/retrieve", async (req, res) => {
-  const blogPosts = [];
-  const doc = await db.collection("blog_posts").doc(req.query.doc_id).get();
-  console.log({...doc.data()})
-  if({...doc.data()}==={})
-    res.sendStatus(200)
-  res.send({...doc.data()});
+app.post("/blog_posts/add", async (req, res) => {
+  const { title, text, pic_url, ...rest } = req.body;
+  const resp = await db.collection("blog_posts").add({
+    title,
+    text,
+    pic_url,
+    num_comments: 0,
+    num_likes: 0,
+  });
+  console.log("Created new blog post with ID: ", resp.id);
+
+  //this is the code for adding a comment to a post, reuse this when getting to comments
+
+  // const resp2 = await db
+  //   .collection("blog_posts")
+  //   .doc(resp.id)
+  //   .collection("comments")
+  //   .add({
+  //     title: "test",
+  //     text: "This is a comment",
+  //     num_likes: 0,
+  //     author: "this should be an id",
+  //   });
+  // console.log("added comment collection to new blog post");
+  res.sendStatus(200);
+});
+
+app.put("/blog_posts/update", async (req, res) => {
+  const { doc_id, title, text, pic_url, ...rest } = req.body;
+
+  const resp = await db.collection("blog_posts").doc(doc_id).update({
+    title,
+    text,
+    pic_url,
+  });
+  res.send("Got a PUT request to update post:" + resp.id);
+});
+
+app.delete("/blog_posts/delete", async (req, res) => {
+  const { doc_id, ...rest } = req.body;
+  const resp = await db.collection("blog_posts").doc(doc_id).delete();
+  console.log("From blog posts, deleted: ", doc_id);
+  res.send("Got a DELETE request");
 });
 
 module.exports = app;
