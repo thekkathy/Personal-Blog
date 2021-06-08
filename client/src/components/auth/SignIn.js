2cd7c47be26e51a5de17947909342c72c986f35a
Dhruv Batra <dhruvbatra2@gmail.com>
2021-06-08 15:33:35 -0400

    sign in page styling

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -2,6 +2,7 @@ import {useState,useEffect,useContext } from "react";
 import { Button } from 'react-bootstrap';
 import { signInWithGoogle,auth } from "../../firebase";
 import {UsersContext} from '../../context/usersContext'
+import Modal from 'react-bootstrap/Modal';
 
 export default function SignIn(){
 
@@ -16,15 +17,20 @@ export default function SignIn(){
   }
 
   return (
-    <div>
-        <Button
-          variant="danger"
-          className="bg-red-500 hover:bg-red-600 w-full py-2 text-white"
-          onClick={() => {
-            handleSignIn();
-          }}>
-          Sign in or Sign Up with Google
-        </Button>
+    <div height='100%' style={{ 
+      backgroundImage: `url("https://i.redd.it/y1ostvqnr4711.jpg")` ,
+      height: '100vh'
+    }}><br></br>
+      <div class="d-flex justify-content-center">
+          <Button
+            variant="danger"
+            class="bg-red-500 hover:bg-red-600 w-full py-2 text-white mx-auto my-auto"
+            onClick={() => {
+              handleSignIn();
+            }}>
+            Sign in or Sign Up with Google
+          </Button>
+      </div>
     </div>
   );
 };
