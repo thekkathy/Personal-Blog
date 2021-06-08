260c6ee9d0ed7b2f96e96d6f5818e1ad17e3c1ae
T-Cal <tcalcuttawala@gmail.com>
2021-06-08 09:35:09 -0400

    testing context

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import BlogCard from './BlogCard';
+import {BlogPostsContext} from "./../../context/blogPostsContext"
+
 
 const Blog = () => {
+    const {blogPosts} = useContext(BlogPostsContext);
+
     return (
         <div>
+            <h1>{JSON.stringify(blogPosts)}</h1>
+            <img src={blogPosts.pic_url} alt='Nothing Found' style={{width:'300px', height:"200px"}}></img>
             Blog
             contains a bunch of blog cards
         </div>
--- client/src/context/blogPostsContext.js
+++ client/src/context/blogPostsContext.js
@@ -3,12 +3,15 @@ import React, { createContext, useState } from "react";
 const BlogPostsContext = createContext();
 
 const BlogPostsProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState(null);
-  
+  const [blogPosts, setBlogPosts] = useState({
+    title: "this is the title",
+    text: "This is a the meat of the blog post. skjhkjsdhkjsdhfkjsdhfk",
+    pic_url:
+      "https://specials-images.forbesimg.com/imageserve/5f85be4ed0acaafe77436710/960x0.jpg?fit=scale",
+  });
+
   return (
-    <BlogPostsContext.Provider
-      value={{ blogPosts, setBlogPosts }}
-    >
+    <BlogPostsContext.Provider value={{ blogPosts, setBlogPosts }}>
       {children}
     </BlogPostsContext.Provider>
   );
