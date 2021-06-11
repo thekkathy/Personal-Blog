3a8ffca526ed112f308cc48543e553997037c1a8
Kathy Kang <kck3due@virginia.edu>
2021-06-10 20:04:47 -0400

    fixed navbar glitch

--- client/src/components/UserPage/UserLikes.js
+++ client/src/components/UserPage/UserLikes.js
@@ -13,30 +13,34 @@ export default function UserLikes() {
     useEffect(() => { getBlogPosts() }, []);
 
     const getBlogPosts = async () => {
-        console.log("fetching blog posts ");
-        const url = new URL("http://localhost:8000/blog_posts/get");
-        let res = await fetch(url).then((resp) => resp.json());
-        console.log(res);
-        setUserLikedPosts(res.filter(el => !!users.liked_posts.toString().match(new RegExp(`(?=.*${el.doc_id})`))))
-        // users.liked_posts.map((post)=> {
-        //     axios.get('http://localhost:8000/blog_posts/retrieve',{
-        //         params:{
-        //             doc_id:post
-        //         }
-        //     })
-        //     .then(function(resp){
-        //         console.log(resp.data)
-        //     })
-        // })
+        if (users) {
+            console.log("fetching blog posts ");
+            const url = new URL("http://localhost:8000/blog_posts/get");
+            let res = await fetch(url).then((resp) => resp.json());
+            console.log("res", res);
+            setUserLikedPosts(res.filter(el => !!users.liked_posts.toString().match(new RegExp(`(?=.*${el.doc_id})`))))
+            console.log("liked posts: ", userLikedPosts);
+            // users.liked_posts.map((post)=> {
+            //     axios.get('http://localhost:8000/blog_posts/retrieve',{
+            //         params:{
+            //             doc_id:post
+            //         }
+            //     })
+            //     .then(function(resp){
+            //         console.log(resp.data)
+            //     })
+            // })
+
+        }
     };
 
     return (
-        <div className='container' style={{height:"40rem"}}>
+        <div className='container' style={{ height: "40rem" }}>
             <div className="row p-4">
                 <h1>Liked Posts</h1>
             </div>
             <div class="row cards">
-                {userLikedPosts.length > 0 ? 
+                {userLikedPosts.length > 0 ?
                     userLikedPosts.map((post) => { return <BlogCard post={post} /> })
                     :
                     <div className="ml-2 h5 font-weight-light">No liked posts yet</div>
